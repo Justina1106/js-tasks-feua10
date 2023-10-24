8c42e92f18f9402f4ffe7a7ce4a9f545fc9a0a66
Justina1106 <jbycenkova@gmail.com>
2023-10-24 20:26:23 +0300

    Task 3 added

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,38 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+const outputDiv = document.getElementById('output');
+const message = document.getElementById('message');
+const button = document.getElementById('btn');
+
+button.addEventListener('click', async () => {
+    // Hide the message
+    message.style.display = 'none';
+
+    try {
+        // Fetch data from the GitHub API
+        const response = await fetch(ENDPOINT);
+        const users = await response.json();
+
+        // Display user information in the output div
+        users.forEach(user => {
+            const card = document.createElement('div');
+            card.classList.add('user-card');
+
+            const username = document.createElement('p');
+            username.textContent = `Username: ${user.login}`;
+
+            const avatar = document.createElement('img');
+            avatar.src = user.avatar_url;
+            avatar.alt = `Avatar for ${user.login}`;
+
+            card.appendChild(username);
+            card.appendChild(avatar);
+            outputDiv.appendChild(card);
+        });
+    } catch (error) {
+         // Handle errors, e.g., display an error message
+         console.error('Error fetching data:', error);
+         outputDiv.textContent = 'Error fetching data. Please try again later.';
+     }
+});
