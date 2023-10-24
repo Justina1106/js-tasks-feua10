e013ac9f4815c8095b3ad86e66f4560c671cb9ac
Justina1106 <jbycenkova@gmail.com>
2023-10-24 21:22:20 +0300

    Task 9 added

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,17 @@ wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada g
 - Jeigu true, tai atsakymą išvesti tokiu formatu: „Režisieriaus {režisieriaus vardas} sukurto filmo {filmo pavadinimas} biudžetas viršijo 100 000 000 mln USD"
 - Jeigu false, tai atsakymą išvesti tokiu formatu: „Režisierius {režisieriaus vardas}, kurdamas filmą {filmo pavadinimas}, neviršijo 100 000 000 mln USD biudžeto"
 ------------------------------------------------------------------------------------------------------ */
+const movie = {
+    title: 'Titanic',
+    director: 'James Cameron',
+    budget: 90000000, 
+    wasExpensive: function() {
+        if (this.budget > 100000000) {
+            return `The budget of the movie ${this.title} made by director ${this.director} exceeded $100,000,000 million.`;
+        } else {
+            return `The director ${this.director} did not exceed a budget of USD 100,000,000 million for the film ${this.title}.`;
+        }
+    }
+};
+
+console.log(movie.wasExpensive());
