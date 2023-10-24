a5cc512b1ab810bbb04e9f0e650982d6defe7119
Justina1106 <jbycenkova@gmail.com>
2023-10-24 21:03:24 +0300

    Task 8 added

--- Task 8/script.js
+++ Task 8/script.js
@@ -9,3 +9,37 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+const calculator = {
+    num1: 0,
+    num2: 0,
+    setNumbers: function(num1, num2) {
+      this.num1 = num1;
+      this.num2 = num2;
+    },
+    sum: function() {
+      return this.num1 + this.num2;
+    },
+    subtraction: function() {
+      return this.num1 - this.num2;
+    },
+    multiplication: function() {
+      return this.num1 * this.num2;
+    },
+    division: function() {
+      // Check if num2 is not 0 to avoid division by zero
+      if (this.num2 !== 0) {
+        return this.num1 / this.num2;
+      } else {
+        return "Cannot divide by zero!";
+      }
+    }
+  };
+  
+ 
+  calculator.setNumbers(10, 5);
+  
+  
+  console.log('Sum:', calculator.sum()); // Output: 15 (10 + 5)
+  console.log('Subtraction:', calculator.subtraction()); // Output: 5 (10 - 5)
+  console.log('Multiplication:', calculator.multiplication()); // Output: 50 (10 * 5)
+  console.log('Division:', calculator.division()); // Output: 2 (10 / 5)
